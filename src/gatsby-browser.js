ececef885e6f6987c8e1917b035ab476f575ed45
bturner-cfn <65620116+bturner-cfn@users.noreply.github.com>
2021-06-15 07:16:37 -0400

    Remove viewcontent tracking event

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,6 +1,7 @@
 exports.onRouteUpdate = function({ location }) {
+  // 06/15/2021 - Making a no-op to troubleshoot analytics counting per marketing.
   // Don't track while developing.
-  if (process.env.NODE_ENV === `production` && typeof fbq === `function`) {
-    fbq('track', 'ViewContent');
-  }
+  // if (process.env.NODE_ENV === `production` && typeof fbq === `function`) {
+  //   fbq('track', 'ViewContent');
+  // }  
 };
